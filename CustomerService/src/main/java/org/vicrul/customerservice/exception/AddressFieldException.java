b9879d49dc026096f3011a3ce51f2f939ff048ab
victor.sotskov <vic.sotskov@gmail.com>
2021-01-14 01:42:46 +0700

    Добавил обработку исключений для обязательных полей: sex, actualAddress и regiusteredAddress сущности Customer

---
+++ CustomerService/src/main/java/org/vicrul/customerservice/exception/AddressFieldException.java
@@ -0,0 +1,9 @@
+package org.vicrul.customerservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Field cannot be empty")
+public class AddressFieldException extends RuntimeException {
+
+}
